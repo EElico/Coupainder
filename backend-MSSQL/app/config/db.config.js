c8ed8cf13e6062a4e37b6c49b8938f1943e64fbf
EElico <eeyalc@gmail.com>
2023-04-08 19:20:11 +0300

    Update db.config.js

--- backend-MSSQL/app/config/db.config.js
+++ backend-MSSQL/app/config/db.config.js
@@ -1,25 +1,9 @@
-// module.exports = {
-//   HOST: "localhost",
-//   PORT: "1434",//edit
-//   USER: "zkoder",//edit
-//   PASSWORD: "123456",//edit
-//   DB: "bezkoder_db",//TODO: edit //#name
-//   dialect: "mssql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
-
 module.exports = {
-  HOST: "database-coupinder.cql4jschedpv.eu-west-3.rds.amazonaws.com",
+  HOST: "localhost",
   PORT: "1433",//edit
-  USER: "admin",//edit
+  USER: "Eli1",//edit
   PASSWORD: "eli123456",//edit
-  DB: "Coupainder",//TODO: edit //#name
+  DB: "coupainder",//TODO: edit //#name
   dialect: "mssql",
   pool: {
     max: 5,
@@ -28,3 +12,19 @@ module.exports = {
     idle: 10000
   }
 };
+
+// #live on aws
+// module.exports = {
+//   HOST: "database-coupinder.cql4jschedpv.eu-west-3.rds.amazonaws.com",
+//   PORT: "1433",
+//   USER: "admin",
+//   PASSWORD: "eli123456",
+//   DB: "Coupainder",
+//   dialect: "mssql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
