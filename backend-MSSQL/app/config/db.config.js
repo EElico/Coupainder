b9256d541b8573f5ffbae3b52e3db3bc1b8b8509
EElico <eeyalc@gmail.com>
2023-04-12 16:44:22 +0300

    12-Apr-23

--- backend-MSSQL/app/config/db.config.js
+++ backend-MSSQL/app/config/db.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   HOST: "localhost",
   PORT: "1433",//edit
-  USER: "Eli1",//edit
+  USER: "sa",//edit
   PASSWORD: "eli123456",//edit
-  DB: "coupainder",//TODO: edit //#name
+  // server:"DESKTOP-DPVPB74",
+  DB: "Coupainder",//TODO: edit //#name
   dialect: "mssql",
+  // driver: 'msnodesqlv8',
   pool: {
     max: 5,
     min: 0,
